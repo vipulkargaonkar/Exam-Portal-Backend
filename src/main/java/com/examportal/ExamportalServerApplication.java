4d1f58746ee05b71431a651f486e8c8258c5967b
vipul.kargaonkar <vipulkargaonkar@gmail.com>
2025-04-21 23:42:58 +0530

    Added starter code

---
+++ src/main/java/com/examportal/ExamportalServerApplication.java
@@ -0,0 +1,13 @@
+package com.examportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamportalServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamportalServerApplication.class, args);
+	}
+
+}
