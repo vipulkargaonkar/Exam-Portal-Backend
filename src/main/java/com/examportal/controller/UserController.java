3af20a7d0b1d2c73ec0d5a830586d1bde4d8e41f
vipul.kargaonkar <vipulkargaonkar@gmail.com>
2025-04-24 21:42:51 +0530

    Added UserController with dummy values

---
+++ src/main/java/com/examportal/controller/UserController.java
@@ -0,0 +1,54 @@
+package com.examportal.controller;
+
+import com.examportal.entity.RolesEntity;
+import com.examportal.entity.UserEntity;
+import com.examportal.entity.UserRoleEntity;
+import com.examportal.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/create")
+    public UserEntity createUser(@RequestBody UserEntity user) throws Exception {
+        Set<UserRoleEntity> roles = new HashSet<>();
+
+        RolesEntity role = new RolesEntity();
+        role.setRoleId(2L);
+        role.setRoleName("NORMAL");
+
+        UserRoleEntity userRole = new UserRoleEntity();
+        userRole.setUser(user);
+        userRole.setRole(role);
+
+        roles.add(userRole);
+
+        return userService.createUser(user, roles);
+    }
+
+    @GetMapping("/{userName}")
+    public UserEntity getUser(@PathVariable("userName") String userName) {
+        return userService.getUser(userName);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable("id") Long id) {
+        userService.deleteUser(id);
+    }
+
+    @PutMapping("/update")
+    public UserEntity updateUser(@RequestBody UserEntity user) {
+        return userService.updateUser(user);
+    }
+
+}
