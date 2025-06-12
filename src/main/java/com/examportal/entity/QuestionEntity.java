42f4afaf59045ce236530aaf68d1522afc2eb02d
vipul.kargaonkar <vipulkargaonkar@gmail.com>
2025-06-12 23:57:07 +0530

    Added @JsonIgnore to answer to hide variable

--- src/main/java/com/examportal/entity/QuestionEntity.java
+++ src/main/java/com/examportal/entity/QuestionEntity.java
@@ -1,5 +1,6 @@
 package com.examportal.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.util.Objects;
@@ -24,6 +25,7 @@ public class QuestionEntity {
 
     private String option4;
 
+    @JsonIgnore
     private String answer;
 
     @Transient
